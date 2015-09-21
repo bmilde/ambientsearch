47dfc31742aa9b1af735fad922612af59771fa42
bmilde <bmilde@users.noreply.github.com>
2015-09-21 11:18:04 +0200

    Fixed wiki keyword search function, added removal of IPA in wiki summaries, wiki api responses are cached

--- python/wiki_search.py
+++ python/wiki_search.py
@@ -1,13 +1,46 @@
 import wikipedia
 
+summary_cache = {}
+keyword_cache = {}
+
+def filterBrackets(test_str):
+    ret = ''
+    skip1c = 0
+    skip2c = 0
+    for i in test_str:
+        if i == '[':
+            skip1c += 1
+        elif i == '(':
+            skip2c += 1
+        elif i == ']' and skip1c > 0:
+            skip1c -= 1
+        elif i == ')'and skip2c > 0:
+            skip2c -= 1
+        elif skip1c == 0 and skip2c == 0:
+            ret += i
+    return ret
+
 def getSummariesSingleKeyword(keywords,lang="en"):
 	wikipedia.set_lang(lang)
-	articles = []
+	articles,summaries = [],[]
 	for keyword in keywords:
-		result = wikipedia.search(keyword)
-		if len(result) > 0:
-			articles.append(result[0])
+		#check cache first
+		if 	keyword in keyword_cache:
+			articles.append(keyword_cache[keyword])
+		else:
+			result = wikipedia.search(keyword)
+			if len(result) > 0:
+				articles.append(result[0])
+				keyword_cache[keyword] = result[0]
+			else:
+				keyword_cache[keyword] = ""
+				
 	for article in articles:
-		summaries = [wikipedia.summary(article, sentences=1)]
-		
+		if article != '':
+			if article in summary_cache:
+				summaries += [summary_cache[article]]
+			else:
+				summary = filterBrackets(wikipedia.summary(article, sentences=1))
+				summaries += [summary]
+				summary_cache[article] = summary
 	return summaries
