6481a1d452c7d35f6b229a97af3d42c8a10421ac
bmilde <bmilde@users.noreply.github.com>
2015-09-22 18:28:05 +0200

    fixed space/tab identation issues in mic_client.py

--- python/mic_client.py
+++ python/mic_client.py
@@ -43,32 +43,32 @@ def rate_limited(maxPerSecond):
 class KaldiClient(WebSocketClient):
 
     def print_devices(self):
-	info = self.paudio.get_host_api_info_by_index(0)
-	numdevices = info.get('deviceCount')
-	#for each audio device, determine if is an input or an output and add it to the appropriate list and dictionary
-	for i in range (0,numdevices):
-	    if self.paudio.get_device_info_by_host_api_device_index(0,i).get('maxInputChannels')>0:
-		print "Input Device id ", i, " - ", self.paudio.get_device_info_by_host_api_device_index(0,i).get('name')
-     
-	    if self.paudio.get_device_info_by_host_api_device_index(0,i).get('maxOutputChannels')>0:
-		print "Output Device id ", i, " - ", self.paudio.get_device_info_by_host_api_device_index(0,i).get('name')
-    
-    def getYamahaID(self):
-	info = self.paudio.get_host_api_info_by_index(0)
-	numdevices = info.get('deviceCount')
-	for i in range (0,numdevices):
-	    if self.paudio.get_device_info_by_host_api_device_index(0,i).get('maxInputChannels')>0:
-		if 'Yamaha' in self.paudio.get_device_info_by_host_api_device_index(0,i).get('name'):
-		    return i
-	print 'No yamaha microphone found, defaulting to first available input device...'
-    
-	for i in range (0,numdevices):
+        info = self.paudio.get_host_api_info_by_index(0)
+        numdevices = info.get('deviceCount')
+        #for each audio device, determine if is an input or an output and add it to the appropriate list and dictionary
+        for i in range (0,numdevices):
             if self.paudio.get_device_info_by_host_api_device_index(0,i).get('maxInputChannels')>0:
-		return i
+                print "Input Device id ", i, " - ", self.paudio.get_device_info_by_host_api_device_index(0,i).get('name')
 
-	print 'No input device found! Please connect a microphone or recording device'
+            if self.paudio.get_device_info_by_host_api_device_index(0,i).get('maxOutputChannels')>0:
+                print "Output Device id ", i, " - ", self.paudio.get_device_info_by_host_api_device_index(0,i).get('name')
 
-	return -1		    
+    def getYamahaID(self):
+        info = self.paudio.get_host_api_info_by_index(0)
+        numdevices = info.get('deviceCount')
+        for i in range (0,numdevices):
+            if self.paudio.get_device_info_by_host_api_device_index(0,i).get('maxInputChannels')>0:
+                if 'Yamaha' in self.paudio.get_device_info_by_host_api_device_index(0,i).get('name'):
+                    return i
+        print 'No yamaha microphone found, defaulting to first available input device...'
+
+        for i in range (0,numdevices):
+            if self.paudio.get_device_info_by_host_api_device_index(0,i).get('maxInputChannels')>0:
+                return i
+
+        print 'No input device found! Please connect a microphone or recording device'
+
+        return -1		    
 
     def __init__(self, filename, url, protocols=None, extensions=None, heartbeat_freq=None, byterate=32000,
                  save_adaptation_state_filename=None, send_adaptation_state_filename=None, keyword_server_url = ''):
@@ -80,16 +80,16 @@ class KaldiClient(WebSocketClient):
         self.save_adaptation_state_filename = save_adaptation_state_filename
         self.send_adaptation_state_filename = send_adaptation_state_filename
 
-	self.paudio = pyaudio.PyAudio()
-	self.print_devices()
-	self.keyword_client = KeywordClient(keyword_server_url)
-	self.send_to_keywordserver = not (keyword_server_url == '')
+        self.paudio = pyaudio.PyAudio()
+        self.print_devices()
+        self.keyword_client = KeywordClient(keyword_server_url)
+        self.send_to_keywordserver = not (keyword_server_url == '')
 
-	self.keyword_extractor = extract.TermExtractor()
-	self.keyword_extractor.filter = extract.permissiveFilter
+        self.keyword_extractor = extract.TermExtractor()
+        self.keyword_extractor.filter = extract.permissiveFilter
 
-	if self.send_to_keywordserver:
-	    self.keyword_client.addUtterance('','speaker1')
+        if self.send_to_keywordserver:
+            self.keyword_client.addUtterance('','speaker1')
             self.last_hyp = ''
 
     #@rate_limited(4)
@@ -100,16 +100,16 @@ class KaldiClient(WebSocketClient):
     def opened(self):
         #print "Socket opened!"
         def send_data_to_ws():
-	    buffer_size = 1024
-	    
-	    yamahaID = self.getYamahaID()
-	    if yamahaID == -1:
-		sys.exit(-1)
-	    else:
-		print 'Selecting device',yamahaID,'as input device'
+            buffer_size = 1024
 
-	    stream = self.paudio.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=1024, input_device_index = yamahaID) #buffer   
-	    #f = open(self.fn, "rb")
+            yamahaID = self.getYamahaID()
+            if yamahaID == -1:
+                sys.exit(-1)
+            else:
+                print 'Selecting device',yamahaID,'as input device'
+
+            stream = self.paudio.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=1024, input_device_index = yamahaID) #buffer   
+            #f = open(self.fn, "rb")
             if self.send_adaptation_state_filename is not None:
                 print >> sys.stderr, "Sending adaptation state from %s" % self.send_adaptation_state_filename
                 try:
@@ -119,9 +119,9 @@ class KaldiClient(WebSocketClient):
                     e = sys.exc_info()[0]
                     print >> sys.stderr, "Failed to send adaptation state: ",  e
             abort = False
-	    while not abort:
+            while not abort:
                 block = stream.read(buffer_size)
-		self.send_data(block)
+                self.send_data(block)
             print >> sys.stderr, "Audio sent, now sending EOS"
             self.send("EOS")
 
@@ -130,70 +130,70 @@ class KaldiClient(WebSocketClient):
 
     def getKeywords(self, currentHyp, contextWords=200, ignoreNumRecentWords=1, maxKeywords=7):
 
-	tokens = nltk.word_tokenize(u' '.join(self.final_hyps) + u' ' + currentHyp)[-contextWords:(None if currentHyp == '' else -ignoreNumRecentWords)] 
-	#tags = nltk.pos_tag(tokens)
-	
-	past_tag = None
-	extracted_keywords = self.keyword_extractor(' '.join(tokens))
-	extracted_keywords = sorted(extracted_keywords, key=lambda x: x[1]*x[2], reverse=True)
-	#print extracted_keywords
-	keywords = [keyword[0] for keyword in extracted_keywords]
-	
-	#Get keywords as a list of nouns. MWEs are heuristically choosen if two preceedings tokens share the same noun tag.
-	#for touple in tags:
-	#    word,tag = touple
-	#    
-	#    if tag in ['NN', 'NNP', 'NNPS', 'NNS']:
-	#	if past_tag and past_tag in ['NN', 'NNP', 'NNPS', 'NNS']:
-	#	    keywords[0] += ' ' + word   
-	#	else:	
-	#	    keywords = [word] + keywords		    
-	#   past_tag = tag
-	#
-	#keywords = [touple[0] for touple in tags if touple[1] in ['NN', 'NNP', 'NNPS', 'NNS']]
-	
-	return keywords[:maxKeywords]
+        tokens = nltk.word_tokenize(u' '.join(self.final_hyps) + u' ' + currentHyp)[-contextWords:(None if currentHyp == '' else -ignoreNumRecentWords)] 
+        #tags = nltk.pos_tag(tokens)
+
+        past_tag = None
+        extracted_keywords = self.keyword_extractor(' '.join(tokens))
+        extracted_keywords = sorted(extracted_keywords, key=lambda x: x[1]*x[2], reverse=True)
+        #print extracted_keywords
+        keywords = [keyword[0] for keyword in extracted_keywords]
+
+        #Get keywords as a list of nouns. MWEs are heuristically choosen if two preceedings tokens share the same noun tag.
+        #for touple in tags:
+        #    word,tag = touple
+        #    
+        #    if tag in ['NN', 'NNP', 'NNPS', 'NNS']:
+        #	if past_tag and past_tag in ['NN', 'NNP', 'NNPS', 'NNS']:
+        #	    keywords[0] += ' ' + word   
+        #	else:	
+        #	    keywords = [word] + keywords		    
+        #   past_tag = tag
+        #
+        #keywords = [touple[0] for touple in tags if touple[1] in ['NN', 'NNP', 'NNPS', 'NNS']]
+
+        return keywords[:maxKeywords]
 
     def received_message(self, m):
-	try:
-	    response = json.loads(str(m))
-	    #print >> sys.stderr, "RESPONSE:", response
-	    #print >> sys.stderr, "JSON was:", m
-	    if response['status'] == 0:
-		if 'result' in response:
-		    trans = response['result']['hypotheses'][0]['transcript']
-		    if response['result']['final']:
-			if trans not in ['a.','I.','i.','the.','but.','one.','it.','she.']:
-			    self.final_hyps.append(trans)
-			    keyword_list = self.getKeywords('')			    
+        try:
+            response = json.loads(str(m))
+            #print >> sys.stderr, "RESPONSE:", response
+            #print >> sys.stderr, "JSON was:", m
+            if response['status'] == 0:
+                if 'result' in response:
+                    trans = response['result']['hypotheses'][0]['transcript']
+                    if response['result']['final']:
+                        if trans not in ['a.','I.','i.','the.','but.','one.','it.','she.']:
+                            self.final_hyps.append(trans)
+                            keyword_list = self.getKeywords('')			    
 
-			    if self.send_to_keywordserver:
-				self.keyword_client.replaceLastUtterance(self.last_hyp, trans, std_speaker)
-				self.keyword_client.addUtterance('',std_speaker)
-				self.last_hyp = ''
-				self.keyword_client.setKeywordList(keyword_list)
-			    print u'\r\033[K',trans.replace(u'\n', u'\\n'),u'        Keywords: [',u','.join(keyword_list),u']' 
-		    else:
-			keyword_list = self.getKeywords(trans)
-			if self.send_to_keywordserver:
-			    self.keyword_client.replaceLastUtterance(self.last_hyp, trans, std_speaker)
-			    self.last_hyp = trans
-			    self.keyword_client.setKeywordList(keyword_list) 
-			print_trans = trans.replace(u'\n', u'\\n')
-			print u'\r\033[K',print_trans,u'        Keywords: [',u','.join(keyword_list),u']'
-		if 'adaptation_state' in response:
-		    if self.save_adaptation_state_filename:
-			print u'Saving adaptation state to %s' % self.save_adaptation_state_filename
-			with open(self.save_adaptation_state_filename, 'w') as f:
-			    f.write(json.dumps(response['adaptation_state']))
-	    else:
-		print  u'Received error from server (status %d)' % response['status']
-		if 'message' in response:
-		    print 'Error message:',  response['message']
-	except Exception:
-	    print 'Exception in received_message'
-	    exc_type, exc_value, exc_traceback = sys.exc_info()
-	    traceback.print_exception(exc_type, exc_value, exc_traceback,
+                            if self.send_to_keywordserver:
+                                self.keyword_client.replaceLastUtterance(self.last_hyp, trans, std_speaker)
+                                self.keyword_client.addUtterance('',std_speaker)
+                                self.last_hyp = ''
+                                self.keyword_client.setKeywordList(keyword_list)
+                            print u'\r\033[K',trans.replace(u'\n', u'\\n'),u'        Keywords: [',u','.join(keyword_list),u']' 
+                    else:
+                        keyword_list = self.getKeywords(trans)
+                        if self.send_to_keywordserver:
+                            self.keyword_client.replaceLastUtterance(self.last_hyp, trans, std_speaker)
+                            self.last_hyp = trans
+                            self.keyword_client.setKeywordList(keyword_list) 
+                        print_trans = trans.replace(u'\n', u'\\n')
+                        print u'\r\033[K',print_trans,u'        Keywords: [',u','.join(keyword_list),u']'
+                if 'adaptation_state' in response:
+                    if self.save_adaptation_state_filename:
+                        print u'Saving adaptation state to %s' % self.save_adaptation_state_filename
+                        with open(self.save_adaptation_state_filename, 'w') as f:
+                            f.write(json.dumps(response['adaptation_state']))
+            else:
+                print  u'Received error from server (status %d)' % response['status']
+                if 'message' in response:
+                    print 'Error message:',  response['message']
+        except Exception:
+            print 'Exception in received_message'
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            traceback.print_exception(exc_type, exc_value, exc_traceback,
                 limit=10, file=sys.stdout)
 
     def get_full_hyp(self, timeout=60):
@@ -207,15 +207,15 @@ class KaldiClient(WebSocketClient):
 def connect_ws(args):
     content_type = args.content_type
     if content_type == '' and args.audiofile == '':
-	   	content_type = "audio/x-raw, layout=(string)interleaved, rate=(int)%d, format=(string)S16LE, channels=(int)1" %(args.rate/2)
-    
+        content_type = "audio/x-raw, layout=(string)interleaved, rate=(int)%d, format=(string)S16LE, channels=(int)1" %(args.rate/2)
+
     ws = KaldiClient('', args.uri + '?%s' % (urllib.urlencode([("content-type", content_type)])), byterate=args.rate,
                   save_adaptation_state_filename=args.save_adaptation_state, send_adaptation_state_filename=args.send_adaptation_state, keyword_server_url=args.ambient_uri)
     ws.connect()
     #print 'Disconnected.'
     result = ws.get_full_hyp()
     print result.encode('utf-8')
-    
+
 def main():
 
     parser = argparse.ArgumentParser(description='Command line client for kaldigstserver and the ambient visualization server')
@@ -227,10 +227,10 @@ def main():
     parser.add_argument('--content-type', default='', help='Use the specified content type (empty by default, for raw files the default is  audio/x-raw, layout=(string)interleaved, rate=(int)<rate>, format=(string)S16LE, channels=(int)1')
     parser.add_argument('--audiofile', help='Audio file to be sent to the server', default='', type=str)
     args = parser.parse_args()
-    
+
     return args
 
-  
+
 if __name__ == "__main__":
     args = main()
     connect_ws(args)
