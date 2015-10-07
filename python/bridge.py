6028df708c6ec37714a56359954efcb2762815ff
bmilde <bmilde@users.noreply.github.com>
2015-10-07 17:31:55 +0200

    Experimental redis only communication

--- python/bridge.py
+++ python/bridge.py
@@ -12,7 +12,7 @@ def idFromTitle(title):
     return title.replace(' ','_').replace("'",'_')
 
 #Abstracts away the details of communicating with the ambient server
-class KeywordClient():
+class KeywordClientHttp():
 
     def __init__(self,server_url):
         self.server_url = server_url
@@ -51,6 +51,43 @@ class KeywordClient():
         red.publish('ambient_transcript_only', json.dumps(data))
         r = requests.post(self.server_url+'reset', data=json.dumps(data), headers=self.request_header)
         return r.status_code
+
+class KeywordClient():
+
+    def __init__(self,server_url):
+        self.server_url = server_url
+        self.request_header = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+    def getSettings():
+        r = requests.get(self.server_url+'getSettings')
+        return r.json()
+        
+    def addRelevantEntry(self, type, title, text, url, score):
+        data = {'handle':'addRelevantEntry','type':type,'entry_id': idFromTitle(title),'title':title,'text':text,'url':url,'score':score, 'insert_before': idFromTitle(title)}
+        red.publish('ambient', json.dumps(data))
+
+    def delRelevantEntry(self, type, title):
+        data = {'handle':'delRelevantEntry','type':type,'title': title, 'entry_id': idFromTitle(title)}
+        red.publish('ambient', json.dumps(data))
+
+    def addUtterance(self, utterance,speaker):
+        data = {'handle':'addUtterance','utterance':utterance,'speaker':speaker}
+        red.publish('ambient', json.dumps(data))
+
+    def replaceLastUtterance(self, old_utterance,new_utterance,speaker):
+        data = {'handle':'replaceLastUtterance','old_utterance':old_utterance,'utterance':new_utterance,'speaker':speaker}
+        red.publish('ambient', json.dumps(data))
+
+    def completeUtterance(self, utterance, speaker):
+        data = {'handle':'completeUtterance','utterance':utterance,'speaker':speaker}
+        red.publish('ambient_transcript_only', json.dumps(data))
+
+    def reset(self):
+        data = {'handle':'reset'}
+        red.publish('ambient_transcript_only', json.dumps(data))
+        r = requests.post(self.server_url+'reset', data=json.dumps(data), headers=self.request_header)
+        return r.status_code
+
         
 #Abstracts away the details of communicating with the ambient server, hacky version
 class KeywordClientHacky():
