d03d9f34907214c741bdabd236f5ebb62db86950
bmilde <bmilde@users.noreply.github.com>
2015-10-12 19:10:46 +0200

    fixed: using the with statement here with the timer in getSummariesSingleKeyword(...) caused exception to hit the try block inside the context manager, aborting the program on a wikipedia error

--- python/wiki_search.py
+++ python/wiki_search.py
@@ -31,44 +31,45 @@ def filterBrackets(test_str):
 # This version does not perform clustering
 #@profile
 def getSummariesSingleKeyword(keywords, max_entries=4, lang='en', pics_folder='pics/'):
-    with Timer() as t:
-        wikipedia.set_lang(lang)
-        articles = []
-        summary_box_info = {}
+    timer = Timer()
+    timer.start()
+    wikipedia.set_lang(lang)
+    articles = []
+    summary_box_info = {}
 
-        num_results = 0
+    num_results = 0
 
-        for keyword,score in keywords:
-            if num_results >= max_entries:
-                break
-            #check cache first
-            if  keyword in keyword_cache:
-                articles.append(keyword_cache[keyword])
-                num_results += 1
+    for keyword,score in keywords:
+        if num_results >= max_entries:
+            break
+        #check cache first
+        if  keyword in keyword_cache:
+            articles.append(keyword_cache[keyword])
+            num_results += 1
+        else:
+            result = wikipedia.search(keyword)
+            if len(result) > 0:
+                try:
+                    article = result[0]
+                    #exclude number articles
+                    if not "(number)" in article and not "Unk"==article:
+                        summary = filterBrackets(wikipedia.summary(article, sentences=1))
+                        articles.append((article,summary,score))
+                        num_results += 1
+                        keyword_cache[keyword] = (article,summary,score)
+                except Exception as e: #TODO: we should jut ignore DisambiguationError and report the rest
+                    pass
             else:
-                result = wikipedia.search(keyword)
-                if len(result) > 0:
-                    try:
-                        article = result[0]
-                        #exclude number articles
-                        if not "(number)" in article and not "Unk"==article:
-                            summary = filterBrackets(wikipedia.summary(article, sentences=1))
-                            articles.append((article,summary,score))
-                            num_results += 1
-                            keyword_cache[keyword] = (article,summary,score)
-                    except Exception as e: #TODO: we should jut ignore DisambiguationError and report the rest
-                        pass
-                else:
-                    keyword_cache[keyword] = ("","",0.0)
-                    
-        for article,summary,score in articles:
-            if article != '':
+                keyword_cache[keyword] = ("","",0.0)
+                
+    for article,summary,score in articles:
+        if article != '':
 
-                if article in wiki_cache:
-                    wiki_article = wiki_cache[article]
-                else:
-                    wiki_article = wikipedia.page(article)
+            if article in wiki_cache:
+                wiki_article = wiki_cache[article]
+            else:
+                wiki_article = wikipedia.page(article)
 
-                summary_box_info[wiki_article.title] = {'title':wiki_article.title,'text':summary,'url':"https://en.wikipedia.org/w/index.php?title="+wiki_article.title.replace(' ','_'),'score':score}
-
+            summary_box_info[wiki_article.title] = {'title':wiki_article.title,'text':summary,'url':"https://en.wikipedia.org/w/index.php?title="+wiki_article.title.replace(' ','_'),'score':score}
+    timer.end()
     return summary_box_info
