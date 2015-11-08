4669252f79e17345e11f57a9c81a6749a0a06b48
bmilde <bmilde@users.noreply.github.com>
2015-11-08 23:53:00 +0100

    fixed blacklist

--- python/relevant_event_generator.py
+++ python/relevant_event_generator.py
@@ -82,8 +82,9 @@ class EventGenerator:
 
         self.blacklist_ids = {}
         if blacklist_file != '':
-            with codecs.open('r',blacklist,'utf-8') as infile:
+            with codecs.open(blacklist_file,'r','utf-8') as infile:
                 for line in infile:
+                    print 'blacklist wiki id:',line[:-1]
                     self.blacklist_ids[line[:-1]] = 1
 
 
@@ -254,13 +255,13 @@ if __name__ == "__main__":
     parser.add_argument('-l', '--language', type=str, default='en', dest='language', help='Select a language for the relevant evant geneartor (en,de). Defaults to en.')
     parser.add_argument('-d', '--decay', type=float, default = 0.95, dest = 'decay', help='Score decay for displayed entries (so that new entries can come to replace the older ones')
     parser.add_argument('-e', '--extra-keywords', type=str, default = '', dest = 'extra_keywords', help='Add these user defined extra keywords (filename)')
-    parser.add_argument('-b', '--blacklist-ids', type=str, default = '', dest = 'bloacklist_ids', help='Never show these wikipedia ids!')
+    parser.add_argument('-b', '--blacklist-ids', type=str, default = '', dest = 'blacklist_file', help='Never show these wikipedia ids!')
 
 
     args = parser.parse_args()
     ke = keyword_extract.KeywordExtract(lang=args.language, extra_keywords=args.extra_keywords)
     ke.buildDruidCache(cutoff_druid_score=args.cutoff_druid_score)
 
-    event_gen = EventGenerator(args.ambient_uri, ke, lang=args.language, decay=args.decay, blacklist_file=args.blacklist)
+    event_gen = EventGenerator(args.ambient_uri, ke, lang=args.language, decay=args.decay, blacklist_file=args.blacklist_file)
     event_gen.start_listen()
 
