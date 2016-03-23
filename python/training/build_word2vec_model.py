4da664aef36e2cbb1d89262d6da0109d157b55d8
Me <me@catchy>
2016-03-23 14:22:13 +0100

    argparse for build_word2vec_model, code cleanup

--- python/training/build_word2vec_model.py
+++ python/training/build_word2vec_model.py
@@ -3,7 +3,7 @@
 __author__ = 'Jonas Wacker'
 
 from gensim.models import Word2Vec
-from os.path import join, dirname, abspath, basename
+from os.path import join, dirname, abspath, basename, exists
 import logging
 import sys
 import time
@@ -12,51 +12,67 @@ import wikidump2text
 import druid
 import nltk
 import codecs
-
-
-def data_directory():
-    return join(dirname(dirname(abspath(__file__))), 'data')
-
-corpus_path = join(data_directory(), 'enwiki-latest-pages-articles12.xml-p001825001p002425000.bz2')
-wiki_text_output_path = join(data_directory(), 'enwiki-latest-pages-articles12.txt')
-word2vec_output_path = join(data_directory(), 'enwiki-latest-pages-articles.word2vec')
-
+import argparse
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 program = basename(sys.argv[0])
 logger = logging.getLogger(program)
 
-# Convert Wikipedia XML dump into .txt format
-# wikidump2text.convert(corpus_path, wiki_text_output_path)
-
-# Load Multiword Expressions as Dictionary
-stopwords_path = join(data_directory(), 'stopwords_en.txt')
-druid_path = join(data_directory(), 'druid_en.bz2')
-druid_dict = druid.DruidDictionary(druid_path, stopwords_path, cutoff_score=0.0)
-
-# Train the word2vec model
-
-
 class MySentences(object):
-    def __init__(self, filename):
+    def __init__(self, filename, druid_dict):
         self.filename = filename
         self.stemmer = nltk.stem.PorterStemmer()
+        self.druid_dict = druid_dict
 
     def __iter__(self):
         # One line contains one wiki article.
         self.corpus = codecs.open(self.filename, 'r', encoding='utf-8')
         for line in self.corpus:
-            ngrams = druid_dict.find_ngrams(line.lower().split())
+            ngrams = self.druid_dict.find_ngrams(line.lower().split())
             yield [self.stemmer.stem(token) for token in ngrams]
 
-sentences = MySentences(wiki_text_output_path)  # a memory-friendly iterator
-# bigram_transformer = Phrases(sentences)
-# logger.info("Finished transforming bigrams. Time needed: " + str(time.time() - start_time))
-start_time = time.time()
-model = Word2Vec(sentences, size=100, window=5, min_count=5, workers=multiprocessing.cpu_count())
+def train_w2v(data_directory, corpus_path, wiki_text_output_path, word2vec_output_path, w2v_dim):
+    start_time = time.time()
+    
+    # Convert Wikipedia XML dump into .txt format
+    if not exists(wiki_text_output_path):
+        logger.info('Converting ' + str(corpus_path) + ' into plain text file: ' + wiki_text_output_path) 
+        wikidump2text.convert(corpus_path, wiki_text_output_path)
 
-# trim unneeded model memory = use(much) less RAM
-model.init_sims(replace=True)
-model.save(word2vec_output_path)
+    # Load Multiword Expressions as Dictionary
+    stopwords_path = join(data_directory, 'stopwords_en.txt')
+    druid_path = join(data_directory, 'druid_en.bz2')
+    druid_dict = druid.DruidDictionary(druid_path, stopwords_path, cutoff_score=0.0)
 
-logger.info("Finished building Word2Vec model. Time needed: " + str(time.time() - start_time))
+    # Train the word2vec model, also use DRUID multiwords
+    sentences = MySentences(wiki_text_output_path, druid_dict)  # a memory-friendly iterator
+    
+    # bigram_transformer = Phrases(sentences)
+    # logger.info("Finished transforming bigrams. Time needed: " + str(time.time() - start_time))
+    
+    model = Word2Vec(sentences, size=w2v_dim, window=5, min_count=5, workers=multiprocessing.cpu_count())
+
+    # trim unneeded model memory = use(much) less RAM
+    model.init_sims(replace=True)
+    model.save(word2vec_output_path)
+
+    logger.info("Finished building Word2Vec model. Time needed: " + str(time.time() - start_time))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('-d', '--data-directory', dest='data_directory', help='Data directory, corpus is read from this directory and the output model is saved here', type=str, default = './data/')
+    parser.add_argument('-c', '--corpus-path', dest='corpus_path', help='Input corpus comment file', type=str, default = 'simplewiki-latest-pages-articles.xml.bz2')
+    parser.add_argument('-w', '--wiki-text-output-path', dest='wiki_text_output_path', help='Wiki text output path (converted from raw wikipedia xml)', type=str, default = 'simplewiki-latest-pages-articles.txt')
+    parser.add_argument('-o', '--word2vec-output-path', dest='word2vec_output_path', help='Output model file', type=str, default = 'simple-enwiki-latest.word2vec')
+    parser.add_argument('-v', '--w2v-dim',  dest='w2v_dim', help='The dimensionality of the word2vec vectors', default=100)
+
+    args = parser.parse_args()
+
+    logger.info('Using data directory: ' + args.data_directory)
+
+    corpus_path = join(args.data_directory, args.corpus_path)
+    wiki_text_output_path = join(args.data_directory, args.wiki_text_output_path)
+    word2vec_output_path = join(args.data_directory, args.word2vec_output_path)
+
+    train_w2v(args.data_directory, corpus_path, wiki_text_output_path, word2vec_output_path, args.w2v_dim)
+
