538baa44b72599be88ec0a5aef12d2035dbc2a7c
bmilde <bmilde+git@gmail.com>
2016-04-05 00:32:25 +0200

    arguments

--- python/training/build_tfidf_wiki_model.py
+++ python/training/build_tfidf_wiki_model.py
@@ -1,12 +1,13 @@
 from gensim.models import TfidfModel
 from gensim import corpora
-from os.path import join, dirname, abspath, basename
+from os.path import join, dirname, abspath, basename, exists
 import nltk
 import logging
 import druid
 import sys
 import codecs
 import time
+import argparse
 
 import wikidump2text
 
@@ -14,55 +15,71 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 program = basename(sys.argv[0])
 logger = logging.getLogger(program)
 
-
-def data_directory():
-    return join(dirname(dirname(abspath(__file__))), 'data')
-
-corpus_path = join(data_directory(), 'enwiki-latest-pages-articles12.xml-p001825001p002425000.bz2')
-wiki_text_output_path = join(data_directory(), 'enwiki-latest-pages-articles12.txt')
-model_output_path = join(data_directory(), 'wiki.tfidf')
-
-stemmer = nltk.stem.PorterStemmer()
-dictionary = corpora.Dictionary()
-
-# Convert Wikipedia XML dump into .txt format
-wikidump2text.convert(corpus_path, wiki_text_output_path)
-
-# Load Multiword Expressions as Dictionary
-stopwords_path = join(data_directory(), 'stopwords_en.txt')
-druid_path = join(data_directory(), 'druid_en.bz2')
-druid_dict = druid.DruidDictionary(druid_path, stopwords_path, cutoff_score=0.0)
-
-
-logger.info("Building tfidf model...")
-start_time = time.time()
-
-
 class TextCorpus(object):
-    def __init__(self, filename):
+    def __init__(self, filename, druid_dict):
         self.corpus = codecs.open(filename, 'r', encoding='utf-8')
+        self.druid_dict = druid_dict
+        self.stemmer = nltk.stem.PorterStemmer()
 
     def __iter__(self):
         # One line contains one wiki article.
         for line in self.corpus:
-            ngrams = druid_dict.find_ngrams(line.lower().split())
-            yield [stemmer.stem(token) for token in ngrams]
-
-articles = TextCorpus(wiki_text_output_path)  # a memory-friendly iterator
-dictionary.add_documents(articles)
-
+            ngrams = self.druid_dict.find_ngrams(line.lower().split())
+            yield [self.stemmer.stem(token) for token in ngrams]
 
 class BowCorpus(object):
-    def __init__(self, filename):
+    def __init__(self, filename, druid_dict, tokenid_dictionary):
         self.corpus = codecs.open(filename, 'r', encoding='utf-8')
+        self.druid_dict = druid_dict
+        self.stemmer = nltk.stem.PorterStemmer()
+        self.tokenid_dictionary = tokenid_dictionary
 
     def __iter__(self):
         for line in self.corpus:
-            ngrams = druid_dict.find_ngrams(line.lower().split())
-            stemmed_article = [stemmer.stem(token) for token in ngrams]
-            yield dictionary.doc2bow(stemmed_article)
+            ngrams = self.druid_dict.find_ngrams(line.lower().split())
+            stemmed_article = [self.stemmer.stem(token) for token in ngrams]
+            yield self.tokenid_dictionary.doc2bow(stemmed_article)
 
-model = TfidfModel(BowCorpus(wiki_text_output_path), id2word=dictionary)
-model.save(model_output_path)
+def build_tfidf_model(data_directory, corpus_path, wiki_text_output_path, model_output_path):
 
-logger.info("Finished building tfidf model. Time needed: " + str(time.time() - start_time))
+    stemmer = nltk.stem.PorterStemmer()
+    tokenid_dictionary = corpora.Dictionary()
+
+    if not exists(wiki_text_output_path):
+        logger.info('Converting ' + str(corpus_path) + ' into plain text file: ' + wiki_text_output_path)
+        # Convert Wikipedia XML dump into .txt format
+        wikidump2text.convert(corpus_path, wiki_text_output_path)
+
+    # Load Multiword Expressions as Dictionary
+    stopwords_path = join(data_directory, 'stopwords_en.txt')
+    druid_path = join(data_directory, 'druid_en.bz2')
+    druid_dict = druid.DruidDictionary(druid_path, stopwords_path, cutoff_score=0.0)
+
+
+    logger.info("Building tfidf model...")
+    start_time = time.time()
+
+    articles = TextCorpus(wiki_text_output_path, druid_dict)  # a memory-friendly iterator
+    tokenid_dictionary.add_documents(articles)
+
+    model = TfidfModel(BowCorpus(wiki_text_output_path, druid_dict, tokenid_dictionary), id2word=tokenid_dictionary)
+    model.save(model_output_path)
+
+    logger.info("Finished building tfidf model. Time needed: " + str(time.time() - start_time))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('-d', '--data-directory', dest='data_directory', help='Data directory, corpus is read from this directory and the output model is saved here', type=str, default = './data/')
+    parser.add_argument('-c', '--corpus-path', dest='corpus_path', help='Input corpus comment file', type=str, default = 'simplewiki-latest-pages-articles.xml.bz2')
+    parser.add_argument('-w', '--wiki-text-output-path', dest='wiki_text_output_path', help='Wiki text output path (converted from raw wikipedia xml)', type=str, default = 'simplewiki-latest-pages-articles.txt')
+    parser.add_argument('-o', '--model-output-path', dest='model_output_path', help='Output model file', type=str, default = 'simple-enwiki-latest.tfidf')
+
+    args = parser.parse_args()
+
+    logger.info('Using data directory: ' + args.data_directory)
+
+    corpus_path = join(args.data_directory, args.corpus_path)
+    wiki_text_output_path = join(args.data_directory, args.wiki_text_output_path)
+    model_output_path = join(args.data_directory, args.model_output_path)
+
+    build_tfidf_model(args.data_directory, corpus_path, wiki_text_output_path, model_output_path)
