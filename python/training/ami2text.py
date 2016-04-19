9c78957c04e5e7a044e47181ad9ea15dadd35828
bmilde <bmilde+git@gmail.com>
2016-04-19 18:27:57 +0200

    added options for convert_ami script

--- python/training/ami2text.py
+++ python/training/ami2text.py
@@ -4,31 +4,37 @@ import codecs
 import logging
 import sys
 
-def data_directory():
-    return os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'data')
+def convert_ami(ami_root_dir, txt_output_dir):
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+    program = os.path.basename(sys.argv[0])
+    logger = logging.getLogger(program)
 
-ami_root_dir = os.path.join(data_directory(), 'ami_raw')
-txt_output_dir = os.path.join(data_directory(), 'ami_transcripts')
+    logger.info('Starting conversion process...')
 
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-program = os.path.basename(sys.argv[0])
-logger = logging.getLogger(program)
+    for myfile in os.listdir(ami_root_dir):
+        if myfile.endswith('.xml'):
+            with codecs.open(os.path.join(ami_root_dir, myfile), 'r', encoding='utf-8', errors='replace') as in_file:
+                raw = in_file.read()
+                tree = ET.fromstring(raw)
+                text = ET.tostring(tree, encoding='utf-8', method='text')
+                output = u' '.join(text.split())
+                filename = os.path.splitext(file)[0]
+                output_file = os.path.join(txt_output_dir, filename + '.txt')
+                with codecs.open(output_file, 'w', encoding='utf-8') as out_file:
+                    out_file.write(output)
 
-logger.info('Starting conversion process...')
+                logger.info(output_file + ' written')
 
-for file in os.listdir(ami_root_dir):
-    if file.endswith('.xml'):
-        with codecs.open(os.path.join(ami_root_dir, file), 'r', encoding='utf-8', errors='replace') as in_file:
-            raw = in_file.read()
-            tree = ET.fromstring(raw)
-            text = ET.tostring(tree, encoding='utf-8', method='text')
-            output = u' '.join(text.split())
-            filename = os.path.splitext(file)[0]
-            output_file = os.path.join(txt_output_dir, filename + '.txt')
-            with codecs.open(output_file, 'w', encoding='utf-8') as out_file:
-                out_file.write(output)
+    logger.info('Conversion done.')
 
-            logger.info(output_file + ' written')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('-a', '--ami-root-dir', dest='ami_root_dir', help='Ami root directory, corpus is read from this directory', type=str, default = './data/ami_raw/')
+    parser.add_argument('-t', '--txt-output-dir', dest='txt_output_dir', help='Txt output directory', type=str, default = './data/ami_transcripts/' )
 
+    args = parser.parse_args()
 
-logger.info('Conversion done.')
+    logger.info('Using ami directory:' + args.ami_root_dir)    
+    logger.info('Output text is saved in:' + args.txt_output_dir)
+
+    convert_ami(args.ami_root_dir, args.txt_output_dir)
