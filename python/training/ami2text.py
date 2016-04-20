ed368d2edba0ff5156cabbeccf738debc72a491d
bmilde <bmilde+git@gmail.com>
2016-04-20 16:44:12 +0200

    ami2text typo in cleaning

--- python/training/ami2text.py
+++ python/training/ami2text.py
@@ -3,12 +3,13 @@ import os
 import codecs
 import logging
 import sys
+import argparse
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+program = os.path.basename(sys.argv[0])
+logger = logging.getLogger(program)
 
 def convert_ami(ami_root_dir, txt_output_dir):
-    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-    program = os.path.basename(sys.argv[0])
-    logger = logging.getLogger(program)
-
     logger.info('Starting conversion process...')
 
     for myfile in os.listdir(ami_root_dir):
@@ -18,7 +19,7 @@ def convert_ami(ami_root_dir, txt_output_dir):
                 tree = ET.fromstring(raw)
                 text = ET.tostring(tree, encoding='utf-8', method='text')
                 output = u' '.join(text.split())
-                filename = os.path.splitext(file)[0]
+                filename = os.path.splitext(myfile)[0]
                 output_file = os.path.join(txt_output_dir, filename + '.txt')
                 with codecs.open(output_file, 'w', encoding='utf-8') as out_file:
                     out_file.write(output)
@@ -29,7 +30,7 @@ def convert_ami(ami_root_dir, txt_output_dir):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('-a', '--ami-root-dir', dest='ami_root_dir', help='Ami root directory, corpus is read from this directory', type=str, default = './data/ami_raw/')
+    parser.add_argument('-a', '--ami-root-dir', dest='ami_root_dir', help='Ami root directory, corpus is read from this directory', type=str, default = './data/ami_raw/words/')
     parser.add_argument('-t', '--txt-output-dir', dest='txt_output_dir', help='Txt output directory', type=str, default = './data/ami_transcripts/' )
 
     args = parser.parse_args()
