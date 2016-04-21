4ee2df4562b4e7de8303848ed7b2fb1e6bc12e2a
bmilde <bmilde+git@gmail.com>
2016-04-21 15:31:32 +0200

    download and model building scripts

--- python/build_models_en.sh
+++ python/build_models_en.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
+cd data/
+if [ ! -f druid_en.bz ]
+then
+    # download English DRUID wiki lookup list
+    wget http://machinelearning.online/nlp/druid/enwiki_mwe_druid_200_1000_sorted_length.bz2
+    mv enwiki_mwe_druid_200_1000_sorted_length.bz2 druid_en.bz2
+fi
+cd ..
+
 # Build conversational model
 
 mkdir data/ami_raw
