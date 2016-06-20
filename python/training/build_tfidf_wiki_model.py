48dd805708167112293811f5690497fa70cccc30
bmilde <bmilde+git@gmail.com>
2016-06-20 11:47:38 +0200

    option to train with and without multiwords, force writing off

--- python/training/build_tfidf_wiki_model.py
+++ python/training/build_tfidf_wiki_model.py
@@ -16,31 +16,47 @@ program = basename(sys.argv[0])
 logger = logging.getLogger(program)
 
 class TextCorpus(object):
-    def __init__(self, filename, druid_dict):
+    def __init__(self, filename, druid_dict, multiwords=True):
         self.corpus = codecs.open(filename, 'r', encoding='utf-8')
         self.druid_dict = druid_dict
         self.stemmer = nltk.stem.PorterStemmer()
+        self.multiwords = multiwords
 
     def __iter__(self):
         # One line contains one wiki article.
         for line in self.corpus:
-            ngrams = self.druid_dict.find_ngrams(line.lower().split())
+            if line[-1] == '\n':
+                line = line[:-1]
+
+            if self.multiwords:
+                ngrams = self.druid_dict.find_ngrams(line.lower().split())
+            else:
+                ngrams = line.lower().split()
+
             yield [self.stemmer.stem(token) for token in ngrams]
 
 class BowCorpus(object):
-    def __init__(self, filename, druid_dict, tokenid_dictionary):
+    def __init__(self, filename, druid_dict, tokenid_dictionary, multiwords=True):
         self.corpus = codecs.open(filename, 'r', encoding='utf-8')
         self.druid_dict = druid_dict
         self.stemmer = nltk.stem.PorterStemmer()
         self.tokenid_dictionary = tokenid_dictionary
+        self.multiwords = multiwords
 
     def __iter__(self):
         for line in self.corpus:
-            ngrams = self.druid_dict.find_ngrams(line.lower().split())
+            if line[-1] == '\n':
+                line = line[:-1]
+    
+            if self.multiwords:
+                ngrams = self.druid_dict.find_ngrams(line.lower().split())
+            else:
+                ngrams = line.lower().split()
+            
             stemmed_article = [self.stemmer.stem(token) for token in ngrams]
             yield self.tokenid_dictionary.doc2bow(stemmed_article)
 
-def build_tfidf_model(data_directory, corpus_path, wiki_text_output_path, model_output_path):
+def build_tfidf_model(data_directory, corpus_path, wiki_text_output_path, model_output_path, multiwords=True):
 
     stemmer = nltk.stem.PorterStemmer()
     tokenid_dictionary = corpora.Dictionary()
@@ -54,14 +70,23 @@ def build_tfidf_model(data_directory, corpus_path, wiki_text_output_path, model_
 
     # Load Multiword Expressions as Dictionary
     stopwords_path = join(data_directory, 'stopwords_en.txt')
-    druid_path = join(data_directory, 'druid_en.bz2')
-    druid_dict = druid.DruidDictionary(druid_path, stopwords_path, cutoff_score=0.2)
+    
+    if multiwords:
+        druid_path = join(data_directory, 'druid_en.bz2')
+        druid_dict = druid.DruidDictionary(druid_path, stopwords_path, cutoff_score=0.2)
 
     logger.info("Building tfidf model...")
     start_time = time.time()
 
-    articles = TextCorpus(wiki_text_output_path, druid_dict)  # a memory-friendly iterator
+    if multiwords:
+        logger.info('Using druid_en.bz2 in  ' + data_directory + ' as multiword dictionary.')
+        articles = TextCorpus(wiki_text_output_path, druid_dict, multiwords=True)  # a memory-friendly iterator
+    else:
+        logger.info('Using no multiword dicitionary, just single words')
+        articles = TextCorpus(wiki_text_output_path, None, multiwords=False)
+    
     tokenid_dictionary.add_documents(articles)
+
 
     model = TfidfModel(BowCorpus(wiki_text_output_path, druid_dict, tokenid_dictionary), id2word=tokenid_dictionary)
     model.save(model_output_path)
@@ -74,6 +99,7 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--corpus-path', dest='corpus_path', help='Input corpus comment file', type=str, default = 'simplewiki-latest-pages-articles.xml.bz2')
     parser.add_argument('-w', '--wiki-text-output-path', dest='wiki_text_output_path', help='Wiki text output path (converted from raw wikipedia xml)', type=str, default = 'simplewiki-latest-pages-articles.txt')
     parser.add_argument('-o', '--model-output-path', dest='model_output_path', help='Output model file', type=str, default = 'simple-enwiki-latest.tfidf')
+    parser.add_argument('-nm', '--no-multiwords', dest='no_multiwords', help='No multiwords in model generation', action='store_true', default=False)
 
     args = parser.parse_args()
 
@@ -83,4 +109,4 @@ if __name__ == '__main__':
     wiki_text_output_path = join(args.data_directory, args.wiki_text_output_path)
     model_output_path = join(args.data_directory, args.model_output_path)
 
-    build_tfidf_model(args.data_directory, corpus_path, wiki_text_output_path, model_output_path)
+    build_tfidf_model(args.data_directory, corpus_path, wiki_text_output_path, model_output_path, not args.no_multiwords)
--- python/training/build_word2vec_model.py
+++ python/training/build_word2vec_model.py
@@ -19,21 +19,26 @@ program = basename(sys.argv[0])
 logger = logging.getLogger(program)
 
 class MySentences(object):
-    def __init__(self, filename, druid_dict):
+    def __init__(self, filename, druid_dict, multiwords=True):
         self.filename = filename
         self.stemmer = nltk.stem.PorterStemmer()
         self.druid_dict = druid_dict
+        self.multiwords = multiwords
 
     def __iter__(self):
         # One line contains one wiki article.
         self.corpus = codecs.open(self.filename, 'r', encoding='utf-8')
         for line in self.corpus:
-            if line[:-1] == '\n':
+            if line[-1] == '\n':
                 line = line[:-1]
-            ngrams = self.druid_dict.find_ngrams(line.lower().split())
+            if self.multiwords:
+                ngrams = self.druid_dict.find_ngrams(line.lower().split())
+            else:
+                # actually just unigrams in this case
+                ngrams = line.lower().split()
             yield [self.stemmer.stem(token) for token in ngrams]
 
-def train_w2v(data_directory, corpus_path, wiki_text_output_path, word2vec_output_path, w2v_dim):
+def train_w2v(data_directory, corpus_path, wiki_text_output_path, word2vec_output_path, w2v_dim, multiwords=True):
     start_time = time.time()
     
     # Convert Wikipedia XML dump into .txt format
@@ -43,20 +48,29 @@ def train_w2v(data_directory, corpus_path, wiki_text_output_path, word2vec_outpu
 
     # Load Multiword Expressions as Dictionary
     stopwords_path = join(data_directory, 'stopwords_en.txt')
-    druid_path = join(data_directory, 'druid_en.bz2')
-    druid_dict = druid.DruidDictionary(druid_path, stopwords_path, cutoff_score=0.2)
 
-    # Train the word2vec model, also use DRUID multiwords
-    sentences = MySentences(wiki_text_output_path, druid_dict)  # a memory-friendly iterator
-    
+    if multiwords:
+        logger.info('Using druid_en.bz2 in  ' + data_directory + ' as multiword dictionary.')
+        druid_path = join(data_directory, 'druid_en.bz2')
+        druid_dict = druid.DruidDictionary(druid_path, stopwords_path, cutoff_score=0.2)
+
+        # Train the word2vec model, also use DRUID multiwords
+        sentences = MySentences(wiki_text_output_path, druid_dict, multiwords=True)  # a memory-friendly iterator
+    else:
+        logger.info('Using no multiword dicitionary, just single words')
+        sentences = MySentences(wiki_text_output_path, None, multiwords=False)
+
     # bigram_transformer = Phrases(sentences)
     # logger.info("Finished transforming bigrams. Time needed: " + str(time.time() - start_time))
     
+    logger.info("Starting model training, will save to: " + word2vec_output_path)
     model = Word2Vec(sentences, size=w2v_dim, window=5, min_count=5, workers=multiprocessing.cpu_count())
 
     # trim unneeded model memory = use(much) less RAM
     model.init_sims(replace=True)
-    model.save(word2vec_output_path)
+    
+    logger.info("Saving to the following path: " + word2vec_output_path)
+    model.save(word2vec_output_path, ignore=[])
 
     logger.info("Finished building Word2Vec model. Time needed: " + str(time.time() - start_time))
 
@@ -77,5 +91,5 @@ if __name__ == '__main__':
     wiki_text_output_path = join(args.data_directory, args.wiki_text_output_path)
     word2vec_output_path = join(args.data_directory, args.word2vec_output_path)
 
-    train_w2v(args.data_directory, corpus_path, wiki_text_output_path, word2vec_output_path, args.w2v_dim)
+    train_w2v(args.data_directory, corpus_path, wiki_text_output_path, word2vec_output_path, args.w2v_dim, not args.no_multiwords)
 
