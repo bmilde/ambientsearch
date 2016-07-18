5524b6c68f51bb9033fd1463f34162ab11bf2113
bmilde <bmilde+git@gmail.com>
2016-07-18 16:27:57 +0200

    new pretrained models for english

--- python/download_models_en.sh
+++ python/download_models_en.sh
@@ -2,17 +2,16 @@
 
 cd data/
 # English DRUID wiki lookup list
-wget http://machinelearning.online/nlp/druid/enwiki_mwe_druid_200_1000_sorted_length.bz2
-mv enwiki_mwe_druid_200_1000_sorted_length.bz2 druid_en.bz2
+wget http://machinelearning.online/ambient_search/druid_en.tar.bz2
 
 # Experimental German DRUID lookup list
 #wget http://machinelearning.online/nlp/druid/denews_mwe_druid_200_1000_scored_sorted_filtered.bz2
 #mv denews_mwe_druid_200_1000_scored_sorted_filtered.bz2 druid_de.bz2
 
 # Ambient Search models
-wget http://machinelearning.online/ambient_search/simple-enwiki-latest-models.tar.bz2
-tar xvfj simple-enwiki-latest-models.tar.bz2
-rm simple-enwiki-latest-models.tar.bz2
+wget http://machinelearning.online/ambient_search/simple_enwiki_latest_may2016_models.tar.bz2
+tar xvfj simple_enwiki_latest_may2016_models.tar.bz2
+rm simple_enwiki_latest_may2016_models.tar.bz2
 wget http://machinelearning.online/ambient_search/conversation.tfidf
 
 cd ..
