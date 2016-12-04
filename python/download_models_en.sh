4592f6a56083a7c39d34159f0cceeee4cb3d82d2
bmilde <bmilde@users.noreply.github.com>
2016-12-04 21:39:16 +0100

    Update download_models_en.sh
    Corrected incorrect path

--- python/download_models_en.sh
+++ python/download_models_en.sh
@@ -2,7 +2,7 @@
 
 cd data/
 # English DRUID wiki lookup list
-wget http://machinelearning.online/ambient_search/druid_en.tar.bz2
+wget http://machinelearning.online/ambient_search/druid_en.bz2
 
 # Experimental German DRUID lookup list
 #wget http://machinelearning.online/nlp/druid/denews_mwe_druid_200_1000_scored_sorted_filtered.bz2
